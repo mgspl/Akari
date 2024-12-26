abdf29683da0d169b4f17ea94bf167bc951d2053
Miguel <117107479+mgspl@users.noreply.github.com>
2024-12-26 15:39:36 -0300

    Update treesitter.nix

--- config/plugins/treesitter/treesitter.nix
+++ config/plugins/treesitter/treesitter.nix
@@ -1,7 +1,30 @@
+{ pkgs, ... }:
 {
   plugins = {
     treesitter = {
       enable = true;
+
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        json
+        lua
+        make
+        markdown
+        nix
+        regex
+        toml
+        vim
+        vimdoc
+        xml
+        yaml
+        qmljs
+        c
+        cpp
+        python
+        hyprlang
+        html
+      ];
+
       settings = {
         highlight.enable = true;
         incremental_selection.enable = true;
